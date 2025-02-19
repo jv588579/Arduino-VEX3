1fca4bdc670309775c51bb099e1b13d536c74b40
jv588579 <villajoshua016@gmail.com>
2025-02-19 13:25:41 -0700

    Update Blink.ino

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -3,10 +3,10 @@ void setup() {
 }
 
 void loop() {
-on(1000, 500); // ON 1s (1000ms), OFF 0.5s (500 ms)
+on(10, 50); // ON 1s (1000ms), OFF 0.5s (500 ms)
 }
 
-void on(int x, y){
+void on(int x,int y){
   digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
   delay(x);
   off(y);
